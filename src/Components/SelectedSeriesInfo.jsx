b72e455450b9501e7c338ba279be8de38a70a65f
AAron Staton <Orson108@hotmail.com>
2020-03-15 18:54:34 -0400

    feature - updated DisplaySeries Component, onClick routes to Theatre Component and sets app to full screen

--- src/Components/SelectedSeriesInfo.jsx
+++ src/Components/SelectedSeriesInfo.jsx
@@ -4,11 +4,20 @@ import React from "react";
 //Component imports
 import { VideoPlayer } from "./";
 
-const SelectedSeriesInfo = ({ title, path }) => {
-  console.log("In series info path is: ", path);
+//other imports
+import { library } from "./../library";
+const electron = window.require("electron");
+const mainWindow = electron.remote.getCurrentWindow();
+
+const SelectedSeriesInfo = ({ title, path, history }) => {
+  console.log("In series inf are: ");
+  const clickHandler = () => {
+    mainWindow.setFullScreen(true);
+    history.push("theatre");
+  };
   return (
-    <div className="SeriesInfoContainer">
-      <VideoPlayer src={path} />
+    <div className="SeriesInfoContainer" onClick={clickHandler}>
+      <VideoPlayer path={path} height="400" width="500" />
       <p>{title}</p>
     </div>
   );
