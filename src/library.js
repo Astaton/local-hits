9983ed78d637f6da8f8c9edc4b2eadabc98935e7
AAron Staton <Orson108@hotmail.com>
2020-03-15 18:06:49 -0400

    fix -in library file, refactored the library info

--- src/library.js
+++ src/library.js
@@ -1,24 +1,55 @@
-export const library = {
-  "Fight Scenes": [
-    {
-      title: "Cowboy Bebop",
-      image: "default.jpg",
-      description: "",
-      path:
-        "/home/aaron/FullStack/Hackathon/localthits1/src/library/Cowboy Bebop fight scene.mp4"
-    }
-  ],
-  "Nature Scenes": [
-    {
-      title: "Beautiful Nature",
-      image: "default.jpg",
-      description: "",
-      path:
-        "/home/aaron/FullStack/Hackathon/localthits1/src/library/beautiful scene of nature.mp4"
-    }
-  ],
+const libraryTemplate = {
   toString() {
-    const series = Object.keys(library);
-    return series.join(", ");
+    const genres = Object.keys(this);
+    return genres.join(", ");
   }
 };
+
+export const library = Object.create(libraryTemplate);
+
+library["Fight Scenes"] = [
+  {
+    title: "Cowboy Bebop",
+    image: "/home/aaron/FullStack/Hackathon/localthits1/src/images/movie.png",
+    description: "",
+    path:
+      "/home/aaron/FullStack/Hackathon/localthits1/src/library/Cowboy Bebop fight scene.mp4"
+  },
+  {
+    title: "Fate Staynight",
+    image: "/home/aaron/FullStack/Hackathon/localthits1/src/images/movie.png",
+    description: "",
+    path:
+      "/home/aaron/FullStack/Hackathon/localthits1/src/library/Fate staynight Archer VS Lancer.mp4"
+  },
+  {
+    title: "Kabaneri of the Iron Fortress",
+    image: "/home/aaron/FullStack/Hackathon/localthits1/src/images/movie.png",
+    description: "",
+    path:
+      "/home/aaron/FullStack/Hackathon/localthits1/src/library/Kabaneri of the Iron Fortress - Mumei.mp4"
+  },
+  {
+    title: "One Punch Man",
+    image: "/home/aaron/FullStack/Hackathon/localthits1/src/images/movie.png",
+    description: "",
+    path:
+      "/home/aaron/FullStack/Hackathon/localthits1/src/library/Saitama vs Genos One Punch Man.mp4"
+  },
+  {
+    title: "Sword Of The Stranger",
+    image: "/home/aaron/FullStack/Hackathon/localthits1/src/images/movie.png",
+    description: "",
+    path:
+      "/home/aaron/FullStack/Hackathon/localthits1/src/library/Sword Of The Stranger - Final battle.mp4"
+  }
+];
+library["Nature Scenes"] = [
+  {
+    title: "Beautiful Nature",
+    image: "/home/aaron/FullStack/Hackathon/localthits1/src/images/movie.png",
+    description: "",
+    path:
+      "/home/aaron/FullStack/Hackathon/localthits1/src/library/beautiful scene of nature.mp4"
+  }
+];
