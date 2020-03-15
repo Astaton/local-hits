eb9b39ae348048c6e5de5f763266220391037614
AAron Staton <Orson108@hotmail.com>
2020-03-15 19:00:28 -0400

    feature -in Theatre, brought in electron and updated controls functionality

--- src/Components/VideoControl.jsx
+++ src/Components/VideoControl.jsx
@@ -1,22 +1,35 @@
+//react imports
 import React, { useState, useEffect } from "react";
 
+//other imports
+import { library } from "./../library";
+const electron = window.require("electron");
+const mainWindow = electron.remote.getCurrentWindow();
+
 const VideoControls = () => {
-  const [videoPlayer, setVideoPlayer] = useState();
+  const [video, setVideo] = useState();
 
   useEffect(() => {
-    const videoPlayer = document.getElementsByTagName("video")[0];
-    setVideoPlayer(videoPlayer);
+    const videoElement = document.getElementsByTagName("video")[0];
+    setVideo(videoElement);
   }, []);
 
-  // const controls: object = {
-  //   play(player: Player){
-  //     player.play()
-  //   }
-  // }
+  const playPauseHandler = () => {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  };
+
+  const fullScreenHandler = () => {
+    mainWindow.setFullScreen(false);
+  };
 
   return (
     <div>
-      <button onClick={() => videoPlayer.play()}>Play</button>
+      <button onClick={playPauseHandler}>Play/Pause</button>
+      <button onClick={fullScreenHandler}>Exit Full Screen</button>
     </div>
   );
 };
